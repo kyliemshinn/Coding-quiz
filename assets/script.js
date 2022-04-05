aa5ef9e95bd0e91352e7d7d25d524d8f6e324734
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-04 18:39:54 -0700

    add timer elements to Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
 // all needed global variables for the game to pull from throughout
-var startButton= document.getElementById("start-btn")
-var questionBox= document.getElementById("question-box")
-var timer= document.getElementById("timer")
-var highscores= document.getElementById("highscores")
+
+//all buttons
+var startButton= document.getElementById("start-btn");
+var questionBox= document.getElementById("question-box");
+
+//for the questions
+var questionEl= document.getElementById("quiz-questions");
+var answerEl= document.getElementById("answer-btns");
+var questionIndex= 0;
+var score=0;
+
+//for highscores
+var highscores= document.getElementById("highscores");
 var calculatedPoints= 0;
+
+//for the timer during the quiz
+var timer= document.getElementById("timer");
+var timerStart= 60;
+var timeRemain= timerStart;
+
+//to get the buttons to shuffle at random
+var shuffledQuestions;
+var currentQuestionIndex;
+
+var end= true;
+
 //questions and answers for each of the quiz questions displayed on the page- nest in objects
 var questions=[
     {
@@ -47,20 +68,51 @@ function startGame () {
 
 startButton.classList.add("hide");
 questionBox.classList.remove("hide");
-nextQuestion();
 
+shuffledQuestions= questions.sort(() => Math.random() - .5);
+currentQuestionIndex = 0;
+
+timer.style.display = `block`;
+
+nextQuestion();
 }
 
+//setting parameters of the timer
+function startTimer() {
+    var timerInterval = setInterval(function() {
+        if(end) { 
+            clearInterval(timerInterval); 
+            return;
+        }
+        if(timeLeft < 1) { 
+            clearInterval(timerInterval); 
+            endGame(); //end game out of time scenario
+        }
+
+        timerTag.textContent = timeLeft; //update timer tag to latest time
+        timeLeft--; //decrement timer after all code runs
+    }, 1000); //1 second intervals
+
+    return;
+}
 
 
 //creating a function to transition to the next question
 function nextQuestion() {
+    displayQuestions(shuffledQuestions[currentQuestionIndex])
 
 } 
 
-
+function displayQuestions(question) {
+    questionEl.innerText=question.question;
+}
 
 //what happens when you select the correct answer
 function selectAnswer() {
+
+}
+
+
+function countDown () {
 
 }
